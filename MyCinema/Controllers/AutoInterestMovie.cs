97f087eeaccc670e8792ae4d48502296a690c9c0
master <ndung983@gmail.com>
2023-09-18 00:35:48 +0700

    lau lau commit

--- MyCinema/Controllers/AutoInterestMovie.cs
+++ MyCinema/Controllers/AutoInterestMovie.cs
@@ -1,192 +1,365 @@
-// using System;
-// using System.Collections.Generic;
-
-
-
-// public class Program
-// {
-//     public static List<MovieSchedule> ScheduleMovies(string[] movieList, TimeSpan maxDuration)
-//     {
-//         List<MovieSchedule> schedule = new List<MovieSchedule>();
-//         DateTime currentTime = DateTime.Parse("2023-09-14 10:00"); // Thời gian bắt đầu chiếu
-
-//         foreach (string movie in movieList)
-//         {
-//             DateTime endTime = currentTime.Add(maxDuration);
-//             schedule.Add(new MovieSchedule(movie, currentTime, endTime));
-
-//             currentTime = endTime.AddMinutes(15); // Cách nhau 15 phút cho chuẩn bị
-//         }
-
-//         return schedule;
-//     }
-
-//     public static void Main()
-//     {
-//         string[] movies = { "Movie A", "Movie B", "Movie C", "Movie D", "Movie E" };
-//         TimeSpan maxDuration = TimeSpan.FromHours(2); // Thời gian tối đa mỗi bộ phim
-
-//         List<MovieSchedule> schedule = ScheduleMovies(movies, maxDuration);
-
-//         // In lịch chiếu đã được xếp
-//         foreach (var item in schedule)
-//         {
-//             Console.WriteLine($"{item.MovieName} - {item.StartTime} - {item.EndTime}");
-//         }
-//     }
-// }
-using System.Resources;
+using System.Runtime.InteropServices;
 using System;
-using System.Net;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using MyCinema.Model;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
-using System.Data.SqlClient;
-using MySql.Data.MySqlClient;
-// using MySql.Data.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
 
-namespace webapiserver.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class AutointerestMovieController : ControllerBase
+namespace webapiserver.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
+    public class AutointerestMovieController : ControllerBase
+    {
         private readonly CinemaContext _context;
+
         public AutointerestMovieController(CinemaContext context)
         {
             _context = context;
-            
         }
 
- public static List<MovieSchedule> GenerateMovieSchedule(string[] movieList, TimeSpan duration, int numberOfShowtimes, List<DateTime> weekDays)
-    {
-        List<MovieSchedule> schedule = new List<MovieSchedule>();
-        DateTime currentTime;
-
-        foreach (DateTime day in weekDays)
-        {
-            currentTime = day;
-
-            foreach (string movie in movieList)
-            {
-                for (int i = 0; i < numberOfShowtimes; i++)
-                {
-                    DateTime endTime = currentTime.Add(duration);
-                    schedule.Add(new MovieSchedule(movie, currentTime, endTime));
-                    currentTime = endTime.AddMinutes(15); // Cách nhau 15 phút cho chuẩn bị
-                }
-
-                currentTime = day; // Reset lại thời gian cho bộ phim tiếp theo
-            }
-        }
-
-        return schedule;
-    }
-
-//  public static List<MovieSchedule> ScheduleMovies(string[] movieList, TimeSpan maxDuration)
-//     {
-//         List<MovieSchedule> schedule = new List<MovieSchedule>();
-//         DateTime currentTime = DateTime.Parse("2023-09-14 10:00"); // Thời gian bắt đầu chiếu
-
-//         foreach (string movie in movieList)
-//         {
-//             DateTime endTime = currentTime.Add(maxDuration);
-//             schedule.Add(new MovieSchedule(movie, currentTime, endTime));
-
-//             currentTime = endTime.AddMinutes(15); // Cách nhau 15 phút cho chuẩn bị
-//         }
-
-//         return schedule;
-//     }
-
-    public static void Main()
-    {
-       
-    }
-
-// API GET LIST VOUCHER
-[HttpGet("getlistvoucher")]
-public IActionResult getListVoucher()
-{
-    // khoi tao api response
-    var successApiResponse = new ApiResponse();
-    //header
-       string token = Request.Headers["token"];
-       string filterHeaderValue2 = Request.Headers["ProjectId"];
-       string filterHeaderValue3 = Request.Headers["Method"];
-       string expectedToken = ValidHeader.Token;
-       string method =Convert.ToString(ValidHeader.MethodGet);
-       string Pojectid = Convert.ToString(ValidHeader.Project_id);
-    //check header
-        if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(filterHeaderValue2) || string.IsNullOrEmpty(filterHeaderValue3))
-        {
-        // The "Authorize" header was not found in the request
-           return BadRequest("Authorize header not found in the request.");
-        }else {
-
-            if (token != expectedToken || filterHeaderValue2 != Pojectid || filterHeaderValue3 != method)
-          {
-            return Unauthorized("Invalid token."); // Return an error response if the tokens don't match
-          }else{
-            // if (date != null && Idmovie != null){
-                
-                  
-               try
-                 {
-                  string[] movies = { "Movie A", "Movie B", "Movie C", "Movie D", "Movie E" };
-        TimeSpan duration = TimeSpan.FromHours(2); // Thời gian mỗi bộ phim
-        int numberOfShowtimes = 5; // Số suất chiếu cho mỗi bộ phim
-        List<DateTime> weekDays = new List<DateTime>();
-
-        // Tạo danh sách các ngày trong tuần
-        DateTime startDate = DateTime.Parse("2023-09-18"); // Thời gian bắt đầu tuần
-
-        for (int i = 0; i < 7; i++)
-        {
-            weekDays.Add(startDate.AddDays(i));
-        }
-
-        List<MovieSchedule> schedule = GenerateMovieSchedule(movies, duration, numberOfShowtimes, weekDays);
-      
-       
-                //       string sql = "call cinema.getListVoucher()";
-                //    var dataget = _context.Vouchers.FromSqlRaw(sql).AsEnumerable().ToList();
-                      successApiResponse.Status = 200;
-                     successApiResponse.Message = "OK";
-                     successApiResponse.Data = schedule;
-                 }
-                 catch (IndexOutOfRangeException ex)
-                  {
-    
-                  }     
-            // }else {
-            //     return BadRequest("khong tim thay thong tin");
-            // }
-                 
-
-           }
-
-        }
- return Ok(successApiResponse);
-}
-
-
-
-}
-
+ 
 public class MovieSchedule
 {
     public string MovieName { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
+}
 
-    public MovieSchedule(string name, DateTime start, DateTime end)
+public class RoomAuto
+{
+    public string Name { get; set; }
+    public List<MovieSchedule> Schedule { get; set; } = new List<MovieSchedule>();
+}
+
+// public class CinemaScheduler
+// {
+//     public static List<RoomAuto> GenerateSchedule(List<string> movieList, List<string> roomList, int durationInMinutes)
+//     {
+//         List<RoomAuto> rooms = new List<RoomAuto>();
+
+//         foreach (string roomName in roomList)
+//         {
+//             RoomAuto room = new RoomAuto { Name = roomName };
+
+//             DateTime currentDate = DateTime.Now.Date;
+//             DateTime endDate = currentDate.AddDays(7);
+
+//             TimeSpan startHour = TimeSpan.FromHours(8); // Thời gian bắt đầu (8 giờ sáng)
+//             TimeSpan endHour = TimeSpan.FromHours(22);  // Thời gian kết thúc (10 giờ tối)
+
+//             DateTime currentTime = currentDate + startHour;
+
+//             while (currentTime.TimeOfDay <= endHour && currentDate <= endDate)
+//             {
+//                 foreach (string movieName in movieList)
+//                 {
+//                     DateTime endTime = currentTime.AddMinutes(durationInMinutes);
+
+//                     if (endTime.TimeOfDay <= endHour)
+//                     {
+//                         MovieSchedule newMovie = new MovieSchedule
+//                         {
+//                             MovieName = movieName,
+//                             StartTime = currentTime,
+//                             EndTime = endTime
+//                         };
+
+//                         room.Schedule.Add(newMovie);
+//                     }
+//                 }
+
+//                 currentTime = currentDate + startHour; // Reset lại thời gian bắt đầu cho ngày tiếp theo
+//                 currentDate = currentDate.AddDays(1); // Chuyển qua ngày tiếp theo
+//             }
+
+//             rooms.Add(room);
+//         }
+
+//         return rooms;
+//     }
+// }
+//    public static List<RoomAuto> GenerateSchedule(List<string> movieList, List<string> roomList, int durationInMinutes)
+//     {
+//         List<RoomAuto> rooms = new List<RoomAuto>();
+
+//         foreach (string roomName in roomList)
+//         {
+//             RoomAuto room = new RoomAuto { Name = roomName };
+
+//             DateTime currentDate = DateTime.Now.Date;
+//             DateTime endDate = currentDate.AddDays(7);
+
+//             TimeSpan startHour = TimeSpan.FromHours(8); // Thời gian bắt đầu (8 giờ sáng)
+//             TimeSpan endHour = TimeSpan.FromHours(22);  // Thời gian kết thúc (10 giờ tối)
+
+//             DateTime currentTime = currentDate + startHour;
+
+//             while (currentTime.TimeOfDay <= endHour && currentDate <= endDate)
+//             {
+//                 foreach (string movieName in movieList)
+//                 {
+//                     DateTime endTime = currentTime.AddMinutes(durationInMinutes);
+
+//                     if (endTime.TimeOfDay <= endHour)
+//                     {
+//                         MovieSchedule newMovie = new MovieSchedule
+//                         {
+//                             MovieName = movieName,
+//                             StartTime = currentTime,
+//                             EndTime = endTime
+//                         };
+
+//                         room.Schedule.Add(newMovie);
+//                     }
+//                 }
+
+//                 currentTime = currentDate + startHour; // Reset lại thời gian bắt đầu cho ngày tiếp theo
+//                 currentDate = currentDate.AddDays(1); // Chuyển qua ngày tiếp theo
+//             }
+
+//             rooms.Add(room);
+//         }
+
+//         return rooms;
+//     }
+
+// public static List<RoomAuto> GenerateSchedule(List<string> movieList, List<string> roomList, int movieDurationMinutes)
+// {
+//     List<RoomAuto> rooms = new List<RoomAuto>();
+
+//     foreach (string roomName in roomList)
+//     {
+//         RoomAuto room = new RoomAuto { Name = roomName };
+
+//         DateTime currentDate = DateTime.Now.Date;
+//         DateTime endDate = currentDate.AddDays(7);
+
+//         while (currentDate <= endDate)
+//         {
+//             DateTime startTime = currentDate.Date + TimeSpan.FromHours(8); // 8:00 AM
+//             DateTime endTime = currentDate.Date + TimeSpan.FromHours(22).Add(TimeSpan.FromMinutes(30)); // 10:30 PM
+
+//             foreach (string movieName in movieList)
+//             {
+//                 DateTime movieStartTime = startTime;
+//                 DateTime movieEndTime = movieStartTime.AddMinutes(movieDurationMinutes);
+
+//                 while (movieEndTime <= endTime)
+//                 {
+//                     MovieSchedule newMovie = new MovieSchedule
+//                     {
+//                         MovieName = movieName,
+//                         StartTime = movieStartTime,
+//                         EndTime = movieEndTime
+//                     };
+
+//                     room.Schedule.Add(newMovie);
+
+//                     movieStartTime = movieEndTime; // Next movie starts after current movie
+//                     movieEndTime = movieStartTime.AddMinutes(movieDurationMinutes);
+//                 }
+//             }
+
+          
+
+//             // Move to the next day
+//             currentDate = currentDate.AddDays(1);
+//         }
+//           rooms.Add(room);
+//     }
+
+//     return rooms;
+// }
+
+public static List<RoomAuto> GenerateSchedule(DateTime dateStart,DateTime dateEnd,List<string> movieList, List<string> roomList, int movieDurationMinutes)
+{
+    Random random = new Random();
+    List<RoomAuto> rooms = new List<RoomAuto>();
+
+    foreach (string roomName in roomList)
     {
-        MovieName = name;
-        StartTime = start;
-        EndTime = end;
+        RoomAuto room = new RoomAuto { Name = roomName };
+
+        DateTime currentDate = dateStart;
+        DateTime endDate = dateEnd;
+
+        while (currentDate <= endDate)
+        {
+            DateTime startTime = currentDate.Date + TimeSpan.FromHours(8); // 8:00 AM
+            DateTime endTime = currentDate.Date + TimeSpan.FromHours(22).Add(TimeSpan.FromMinutes(30)); // 10:30 PM
+          
+            
+            while (startTime.AddMinutes(movieDurationMinutes) <= endTime)
+            {
+                  string randomMovie = movieList[random.Next(movieList.Count)]; // Chọn ngẫu nhiên một bộ phim
+                 string[] parts = randomMovie.Split('/'); // Tách chuỗi theo ký tự '/'
+                int movieDurationMinutess = Convert.ToInt32(parts[1]); // Chuyển đổi phần tử thứ hai thành số nguyên
+                DateTime movieEndTime = startTime.AddMinutes(movieDurationMinutess);
+
+                MovieSchedule newMovie = new MovieSchedule
+                {
+                    MovieName = randomMovie,
+                    StartTime = startTime,
+                    EndTime = movieEndTime
+                };
+
+                room.Schedule.Add(newMovie);
+
+                startTime = movieEndTime; // Next movie starts after current movie
+            }
+
+           
+            // Move to the next day
+            currentDate = currentDate.AddDays(1);
+        }
+         rooms.Add(room);
+
     }
+
+    return rooms;
+}
+
+// public static List<RoomAuto> GenerateSchedule( DateTime startDate, DateTime endDate,List<string> movieList, List<string> roomList)
+// {
+//     List<RoomAuto> rooms = new List<RoomAuto>();
+//     Random random = new Random();
+
+//     int roomIndex = 0;
+
+//     for (DateTime currentDate = startDate; currentDate <= endDate; currentDate = currentDate.AddDays(1))
+//     {
+//         RoomAuto room = new RoomAuto { Name = roomList[roomIndex % roomList.Count] }; // Lặp lại các rạp
+//         roomIndex++;
+
+//         DateTime currentTime = currentDate + TimeSpan.FromHours(8); // Bắt đầu từ 8h sáng
+
+//         while (currentTime.TimeOfDay <= TimeSpan.FromHours(22.5)) // 22h30
+//         {
+//             string randomMovie = movieList[random.Next(movieList.Count)];
+//             string[] parts = randomMovie.Split('/');
+//             int movieDurationMinutes = Convert.ToInt32(parts[1]);
+
+//             DateTime movieEndTime = currentTime.AddMinutes(movieDurationMinutes);
+//             if (movieEndTime.TimeOfDay <= TimeSpan.FromHours(22.5))
+//             {
+//                 MovieSchedule newMovie = new MovieSchedule
+//                 {
+//                     MovieName = randomMovie,
+//                     StartTime = currentTime,
+//                     EndTime = movieEndTime
+//                 };
+
+//                 room.Schedule.Add(newMovie);
+//                 currentTime = movieEndTime;
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+
+//         rooms.Add(room);
+//     }
+
+//     return rooms;
+// }
+
+
+
+
+
+
+
+
+[HttpPost("getlistvoucher")]
+public IActionResult GetListVoucher([FromBody] MovieRoomLists datas)
+{
+    var successApiResponse = new ApiResponse();
+
+    string token = Request.Headers["token"];
+    string filterHeaderValue2 = Request.Headers["ProjectId"];
+    string filterHeaderValue3 = Request.Headers["Method"];
+    string expectedToken = ValidHeader.Token;
+    string method = Convert.ToString(ValidHeader.MethodPost);
+    string Pojectid = Convert.ToString(ValidHeader.Project_id);
+
+    if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(filterHeaderValue2) || string.IsNullOrEmpty(filterHeaderValue3))
+    {
+        return BadRequest("Authorize header not found in the request.");
+    }
+    else
+    {
+        if (token != expectedToken || filterHeaderValue2 != Pojectid || filterHeaderValue3 != method)
+        {
+            return Unauthorized("Invalid token.");
+        }
+        else
+        {
+            try
+            {
+    //   List<string> movieList = new List<string>
+    //     {
+    //         "Movie 1",
+    //         "Movie 2",
+    //         "Movie 3"
+    //     };
+        
+
+    //     List<string> roomList = new List<string>
+    //     {
+    //         "Room A",
+    //         "Room B",
+    //         "Room C"
+    //     };
+
+        int durationInMinutes = 120; // Độ dài mỗi buổi phim (2 giờ)
+
+         List<string> movieList = datas.MovieList;
+        List<string> roomList = datas.RoomList;
+        DateTime dayStart = datas.dayStart;
+DateTime dayEnd = datas.dayEnd;
+       var schedule = GenerateSchedule(dayStart,dayEnd,movieList, roomList,durationInMinutes);
+
+///////
+// List<MovieSchedule> movieList = new List<MovieSchedule>
+// {
+//     new MovieSchedule { MovieName = "Movie 1", StartTime = DateTime.Now.AddHours(1), EndTime = DateTime.Now.AddHours(3) },
+//     new MovieSchedule { MovieName = "Movie 2", StartTime = DateTime.Now.AddHours(4), EndTime = DateTime.Now.AddHours(6) }
+// };
+
+// List<string> roomList = new List<string>
+// {
+//     "Room A",
+//     "Room B"
+// };
+
+// List<RoomAuto> schedule = GenerateSchedule(movieList, roomList,120);
+
+
+                successApiResponse.Status = 200;
+                successApiResponse.Message = "OK";
+                successApiResponse.Data = schedule;
+            }
+            catch (Exception ex)
+            {
+                successApiResponse.Status = 500;
+                successApiResponse.Message = "Internal Server Error";
+                successApiResponse.Data = ex.Message;
+            }
+        }
+    }
+    return Ok(successApiResponse);
+}
+
+    }
+
+  public class MovieRoomLists
+{
+    public DateTime dayStart {get;set;}
+     public DateTime dayEnd {get;set;}
+    public List<string> MovieList { get; set; }
+    public List<string> RoomList { get; set; }
+}
 }
