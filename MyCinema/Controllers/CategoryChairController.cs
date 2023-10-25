1a0d12272dcdb7d289471412793c26b5446f92a6
Nguyen Xuan Tien Dung <>
2023-10-25 16:12:05 +0700

    ok

--- MyCinema/Controllers/CategoryChairController.cs
+++ MyCinema/Controllers/CategoryChairController.cs
@@ -25,10 +25,9 @@ public class CategoryChairController : ControllerBase
         }
 
 
-
 // API GET LIST CHAIR in room
-[HttpGet("getlistCategoryChairByRoom")]
-public IActionResult getlistCategoryChairByRoom(long Idroom)
+[HttpGet("getDetailCategoryChair")]
+public IActionResult getDetailCategoryChair(int idcategory)
 {
     // khoi tao api response
     var successApiResponse = new ApiResponse();
@@ -50,13 +49,184 @@ public IActionResult getlistCategoryChairByRoom(long Idroom)
           {
             return Unauthorized("Invalid token."); // Return an error response if the tokens don't match
           }else{
-            if ( Idroom != null ){
+           
+                
+                  
+               try
+                 {
+                     categorychairs category = new categorychairs();
+                     var dataget = _context.Categorychairs.Where(x=>x.Idcategorychair == idcategory).SingleOrDefault();
+                     category.colorchair = dataget.Colorchair;
+                     category.idcategoryChair = dataget.Idcategorychair;
+                     category.namecategory = dataget.Namecategorychair;
+                     category.price = dataget.Price;
+                     category.idroom = dataget.Idroom;
+                     var dataroom = _context.Rooms.Where(x=>x.Idroom == dataget.Idroom).SingleOrDefault();
+                     category.nameroom = dataroom.Nameroom;
+                      successApiResponse.Status = 200;
+                     successApiResponse.Message = "OK";
+                     successApiResponse.Data = category;
+                 }
+                 catch (IndexOutOfRangeException ex)
+                  {
+    
+                  }     
+           
+                 
+
+           }
+
+        }
+ return Ok(successApiResponse);
+}
+
+public class listidchair {
+   public int idchair {get;set;}
+}
+
+public class categorychairofroom {
+   public int idcategory {get;set;}
+    public List<listidchair> listchair {get;set;}
+}
+
+// API GET LIST CHAIR in room
+[HttpPost("updateCategoryChairInChairWithRoom")]
+public IActionResult updateCategoryChairInChairWithRoom([FromBody] categorychairofroom cate)
+{
+    // khoi tao api response
+    var successApiResponse = new ApiResponse();
+    //header
+       string token = Request.Headers["token"];
+       string filterHeaderValue2 = Request.Headers["ProjectId"];
+       string filterHeaderValue3 = Request.Headers["Method"];
+       string expectedToken = ValidHeader.Token;
+       string method =Convert.ToString(ValidHeader.MethodPost);
+       string Pojectid = Convert.ToString(ValidHeader.Project_id);
+    //check header
+        if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(filterHeaderValue2) || string.IsNullOrEmpty(filterHeaderValue3))
+        {
+        // The "Authorize" header was not found in the request
+           return BadRequest("Authorize header not found in the request.");
+        }else {
+
+            if (token != expectedToken || filterHeaderValue2 != Pojectid || filterHeaderValue3 != method)
+          {
+            return Unauthorized("Invalid token."); // Return an error response if the tokens don't match
+          }else{
+           
+                
+                  
+               try
+                 {
+                  List<Chair> chairlist = new List<Chair>();
+                     foreach (var item in  cate.listchair ) {
+                        var datachair = _context.Chairs.Find(item.idchair);
+                        datachair.Idcategorychair = cate.idcategory;
+                        _context.Chairs.Update(datachair);
+                        _context.SaveChanges();
+                        chairlist.Add(datachair);
+                     }
+                     successApiResponse.Status = 200;
+                     successApiResponse.Message = "OK";
+                     successApiResponse.Data = chairlist;
+                 }
+                 catch (IndexOutOfRangeException ex)
+                  {
+    
+                  }     
+           
+                 
+
+           }
+
+        }
+ return Ok(successApiResponse);
+}
+
+// API GET LIST CHAIR in room
+[HttpPost("updateCategoryChairInfo")]
+public IActionResult updateCategoryChairInfo([FromBody] categorychairs cate)
+{
+    // khoi tao api response
+    var successApiResponse = new ApiResponse();
+    //header
+       string token = Request.Headers["token"];
+       string filterHeaderValue2 = Request.Headers["ProjectId"];
+       string filterHeaderValue3 = Request.Headers["Method"];
+       string expectedToken = ValidHeader.Token;
+       string method =Convert.ToString(ValidHeader.MethodPost);
+       string Pojectid = Convert.ToString(ValidHeader.Project_id);
+    //check header
+        if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(filterHeaderValue2) || string.IsNullOrEmpty(filterHeaderValue3))
+        {
+        // The "Authorize" header was not found in the request
+           return BadRequest("Authorize header not found in the request.");
+        }else {
+
+            if (token != expectedToken || filterHeaderValue2 != Pojectid || filterHeaderValue3 != method)
+          {
+            return Unauthorized("Invalid token."); // Return an error response if the tokens don't match
+          }else{
+           
+                
+                  
+               try
+                 {
+                     var dataupdate = _context.Categorychairs.Find(cate.idcategoryChair);
+                     dataupdate.Colorchair = cate.colorchair;
+                     dataupdate.Idroom = cate.idroom;
+                     dataupdate.Namecategorychair = cate.namecategory;
+                      dataupdate.Price = cate.price;
+                      successApiResponse.Status = 200;
+                      _context.Categorychairs.Update(dataupdate);
+                      _context.SaveChanges();
+                     successApiResponse.Message = "OK";
+                     successApiResponse.Data = dataupdate;
+                 }
+                 catch (IndexOutOfRangeException ex)
+                  {
+    
+                  }     
+           
+                 
+
+           }
+
+        }
+ return Ok(successApiResponse);
+}
+
+// API GET LIST CHAIR in room
+[HttpGet("getlistCategoryChairByRoom")]
+public IActionResult getlistCategoryChairByRoom()
+{
+    // khoi tao api response
+    var successApiResponse = new ApiResponse();
+    //header
+       string token = Request.Headers["token"];
+       string filterHeaderValue2 = Request.Headers["ProjectId"];
+       string filterHeaderValue3 = Request.Headers["Method"];
+       string expectedToken = ValidHeader.Token;
+       string method =Convert.ToString(ValidHeader.MethodGet);
+       string Pojectid = Convert.ToString(ValidHeader.Project_id);
+    //check header
+        if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(filterHeaderValue2) || string.IsNullOrEmpty(filterHeaderValue3))
+        {
+        // The "Authorize" header was not found in the request
+           return BadRequest("Authorize header not found in the request.");
+        }else {
+
+            if (token != expectedToken || filterHeaderValue2 != Pojectid || filterHeaderValue3 != method)
+          {
+            return Unauthorized("Invalid token."); // Return an error response if the tokens don't match
+          }else{
+           
                 
                   
                try
                  {
                      
-                   var dataget = _context.Categorychairs.Where(x=> x.Idroom == Idroom).ToList();
+                   var dataget = _context.Categorychairs.ToList();
                       successApiResponse.Status = 200;
                      successApiResponse.Message = "OK";
                      successApiResponse.Data = dataget;
@@ -65,9 +235,72 @@ public IActionResult getlistCategoryChairByRoom(long Idroom)
                   {
     
                   }     
-            }else {
-                return BadRequest("khong tim thay thong tin");
-            }
+           
+                 
+
+           }
+
+        }
+ return Ok(successApiResponse);
+}
+public class categorychairs {
+   public int? idcategoryChair {get;set;}
+   public int? price {get;set;}
+   public string namecategory {get;set;}
+   
+   public string colorchair {get;set;}
+
+   public long? idroom {get;set;}
+
+   public string nameroom {get;set;}
+}
+
+// API GET LIST CHAIR in room
+[HttpPost("createCategoryChair")]
+public IActionResult createCategoryChair([FromBody] categorychairs categorychairinfo)
+{
+    // khoi tao api response
+    var successApiResponse = new ApiResponse();
+    //header
+       string token = Request.Headers["token"];
+       string filterHeaderValue2 = Request.Headers["ProjectId"];
+       string filterHeaderValue3 = Request.Headers["Method"];
+       string expectedToken = ValidHeader.Token;
+       string method =Convert.ToString(ValidHeader.MethodPost);
+       string Pojectid = Convert.ToString(ValidHeader.Project_id);
+    //check header
+        if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(filterHeaderValue2) || string.IsNullOrEmpty(filterHeaderValue3))
+        {
+        // The "Authorize" header was not found in the request
+           return BadRequest("Authorize header not found in the request.");
+        }else {
+
+            if (token != expectedToken || filterHeaderValue2 != Pojectid || filterHeaderValue3 != method)
+          {
+            return Unauthorized("Invalid token."); // Return an error response if the tokens don't match
+          }else{
+           
+                
+                  
+               try
+                 {
+                  var categorychair = new Categorychair();
+                  categorychair.Colorchair = categorychairinfo.colorchair;
+                  categorychair.Namecategorychair = categorychairinfo.namecategory;
+                  categorychair.Price = categorychairinfo.price;
+                  categorychair.Idroom = categorychairinfo.idroom;
+                     _context.Categorychairs.Add(categorychair);
+                     _context.SaveChanges();
+                  
+                      successApiResponse.Status = 200;
+                     successApiResponse.Message = "OK";
+                     successApiResponse.Data = categorychair;
+                 }
+                 catch (IndexOutOfRangeException ex)
+                  {
+    
+                  }     
+           
                  
 
            }
--- MyCinema/Controllers/ChairController.cs
+++ MyCinema/Controllers/ChairController.cs
@@ -23,6 +23,76 @@ public class ChairController : ControllerBase
             _context = context;
             
         }
+public class categoryinfo {
+      public int Idcategorychair { get; set; }
+
+    public string? Namecategorychair { get; set; }
+
+    public string? Colorchair { get; set; }
+
+    public int? Price { get; set; }
+
+    public long? Idroom {get;set;}
+}
+
+// API GET LIST CATEGORY IN ROOM
+[HttpGet("GetListCategoryInRoom")]
+public IActionResult GetListCategoryInRoom(long Idroom)
+{
+    // khoi tao api response
+    var successApiResponse = new ApiResponse();
+    //header
+       string token = Request.Headers["token"];
+       string filterHeaderValue2 = Request.Headers["ProjectId"];
+       string filterHeaderValue3 = Request.Headers["Method"];
+       string expectedToken = ValidHeader.Token;
+       string method =Convert.ToString(ValidHeader.MethodGet);
+       string Pojectid = Convert.ToString(ValidHeader.Project_id);
+    //check header
+        if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(filterHeaderValue2) || string.IsNullOrEmpty(filterHeaderValue3))
+        {
+        // The "Authorize" header was not found in the request
+           return BadRequest("Authorize header not found in the request.");
+        }else {
+
+            if (token != expectedToken || filterHeaderValue2 != Pojectid || filterHeaderValue3 != method)
+          {
+            return Unauthorized("Invalid token."); // Return an error response if the tokens don't match
+          }else{
+            if (Idroom != null){
+                
+                  
+               try
+                 {
+                  List<categoryinfo> chairdata = new List<categoryinfo>(); 
+                      string sql = "select distinct(ch.Idcategorychair),ch.Colorchair,ch.price,ch.Idroom,ch.Namecategorychair from cinema.chair cs inner join cinema.categorychair ch on ch.Idcategorychair = cs.Idcategorychair where cs.Idroom = '"+Idroom+"' ";
+                   var dataget = _context.Categorychairs.FromSqlRaw(sql).AsEnumerable().ToList();
+                 foreach (var item in dataget) {
+                  var datagetinfoCategory = _context.Categorychairs.Where(x=>x.Idcategorychair == item.Idcategorychair).SingleOrDefault();
+                  var categoryinfos = new categoryinfo();
+                  categoryinfos.Colorchair = datagetinfoCategory.Colorchair;
+                  categoryinfos.Namecategorychair = datagetinfoCategory.Namecategorychair;
+                  categoryinfos.Price = datagetinfoCategory.Price;
+                  chairdata.Add(categoryinfos);
+                 }
+                      successApiResponse.Status = 200;
+                     successApiResponse.Message = "OK";
+                     successApiResponse.Data = chairdata;
+                 }
+                 catch (IndexOutOfRangeException ex)
+                  {
+    
+                  }     
+            }else {
+                return BadRequest("khong tim thay thong tin");
+            }
+                 
+
+           }
+
+        }
+ return Ok(successApiResponse);
+}
 
 // API GET LIST CHAIR
 [HttpGet("getlistchair")]
@@ -64,7 +134,8 @@ public IActionResult getListVoucher(long Idroom,long idcinema,int idinterest)
                         NumberChair = item.NumberChair,
                         bill = item.bill,
                         price = datagetprice.Price,
-                        Idcategorychair = item.Idcategorychair
+                        Idcategorychair = item.Idcategorychair,
+                        colorchair = datagetprice.Colorchair
                      };
                      chairdata.Add(chairs);
                    }
@@ -85,6 +156,22 @@ public IActionResult getListVoucher(long Idroom,long idcinema,int idinterest)
 
         }
  return Ok(successApiResponse);
+}
+public class chairresponse {
+       public int Idchair { get; set; }
+
+    public int? NumberChair { get; set; }
+
+    public int? Roww { get; set; }
+
+    public long? Idroom { get; set; }
+
+    public int? StatusChair { get; set; }
+
+    public int? Idcategorychair { get; set; }
+
+    public string? RowChar { get; set; }
+    public string colorchair {get;set;}
 }
 
 // API GET LIST CHAIR in room
@@ -116,11 +203,23 @@ public IActionResult getlistchairRoom(long Idroom)
                   
                try
                  {
-                     
+                    List<chairresponse>  chairresponse = new List<chairresponse>();
                    var dataget = _context.Chairs.Where(x=> x.Idroom == Idroom).ToList();
+                   foreach (var item in dataget) {
+                     var chairitem = new chairresponse();
+                     var datagetchair = _context.Categorychairs.Where(x=>x.Idcategorychair == item.Idcategorychair).SingleOrDefault();
+                     chairitem.Idchair = item.Idchair;
+                     chairitem.Idroom = item.Idroom;
+                      chairitem.NumberChair = item.NumberChair;
+                      chairitem.RowChar = item.RowChar;
+                      chairitem.StatusChair = item.StatusChair;
+                      chairitem.Idcategorychair = item.Idcategorychair;
+                      chairitem.colorchair = datagetchair.Colorchair;
+                      chairresponse.Add(chairitem);
+                   }
                       successApiResponse.Status = 200;
                      successApiResponse.Message = "OK";
-                     successApiResponse.Data = dataget;
+                     successApiResponse.Data = chairresponse;
                  }
                  catch (IndexOutOfRangeException ex)
                   {
@@ -224,6 +323,8 @@ public partial class CHAIRNEW
      public long? bill {get;set;} 
      public int Idcategorychair {get;set;}
      public int? price {get;set;}
+
+     public string colorchair {get;set;}
     
 }
 
